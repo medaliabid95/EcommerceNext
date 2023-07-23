5852467a5e0481d4345be33aa9fea2b8d242f9b0
MoNourTrabelsi <trabelsimednour@gmail.com>
2023-07-23 17:34:26 +0100

    last

--- socket/socket.js
+++ socket/socket.js
@@ -15,18 +15,11 @@ activeUsers=[]
 io.on("connection", (socket) => {
     // add new User
     socket.on("new-user-add", (newUserId) => {
-        // if user is not added previously
-        if (!activeUsers.some((user) => user.userId === newUserId)) {
-            activeUsers.push({ userId: newUserId, socketId: socket.id });
-            console.log("New User Connected", `${socket.id}`);
-        }
         // send all active users to new user
         io.emit("get-users", activeUsers);
     });
 
     socket.on("disconnect", () => {
-        // remove user from active users
-        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
         console.log("User Disconnected", activeUsers);
         // send all active users to all users
         io.emit("get-users", activeUsers);
